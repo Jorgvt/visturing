930d4deb5a49638c0a6c06a4edfa5e548fd5683f
Jorgvt <vila_tronik@hotmail.com>
2025-12-15 13:04:48 +0100

    created tests for differentiability

---
+++ test/jax/test_8.py
@@ -0,0 +1,19 @@
+import jax
+from jax import random, numpy as jnp
+
+from visturing.properties.jax import prop8
+
+
+def test_differentiability():
+    def loss_fn(params):
+        def calculate_diffs(img1, img2):
+            return params*((img1-img2).mean(axis=(1,2,3))**2)**(1/2)
+        output = prop8.evaluate(calculate_diffs,
+                                            data_path='../../Data/Experiment_8',
+                                            gt_path='../../Data/ground_truth/')
+        return output["correlations"]["pearson"]
+
+    params = jnp.array([1.])
+    loss, grad = jax.value_and_grad(loss_fn)(params)
+    assert not jnp.isnan(loss)
+    assert not jnp.isnan(grad)
