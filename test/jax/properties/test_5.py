d3fcc997425871e2c06e6eeca26fda78169a85be
Jorgvt <vila_tronik@hotmail.com>
2026-01-28 17:12:18 +0100

    fixed test error

--- test/jax/properties/test_5.py
+++ test/jax/properties/test_5.py
@@ -1,5 +1,6 @@
 import jax
 from jax import random, numpy as jnp
+import flax.linen as nn
 
 from visturing.properties.jax import prop5
 
@@ -39,3 +40,41 @@ def test_jit_differentiability():
     assert not jnp.isnan(params)
     assert not jnp.isnan(loss)
     assert not jnp.isnan(grad)
+
+
+def test_jit_differentiability_simple():
+    def step(params):
+        def loss_fn(params):
+            def calculate_diffs(img1, img2):
+                p1 = model.apply({"params": params}, img1)
+                p2 = model.apply({"params": params}, img2)
+                return ((p1-p2).mean(axis=(1,2,3))**2+1e-6)**(1/2)
+            output = prop5.evaluate(calculate_diffs,
+                                                data_path='../../../Data/Experiment_5',
+                                                gt_path='../../../Data/ground_truth/')
+            return output["correlations"]["pearson"]
+
+        loss, grad = jax.value_and_grad(loss_fn)(params)
+        params = jax.tree_util.tree_map(lambda x,g: x- 0.01 * g, params, grad)
+        return params, loss, grad
+
+    model = nn.Conv(1, (3,3))
+    variables = model.init(random.PRNGKey(0), jnp.ones((1,256,256,3)))
+    params = variables['params']
+    params, loss, grad = jax.jit(step)(params)
+
+    assert not jnp.isnan(loss)
+    assert not jnp.isnan(
+                jnp.array(
+                    jnp.concatenate(
+                        [a.ravel() for a in jax.tree_util.tree_leaves(grad)]
+                    )
+                )
+            ).all()
+    assert not jnp.isnan(
+                jnp.array(
+                    jnp.concatenate(
+                        [a.ravel() for a in jax.tree_util.tree_leaves(params)]
+                    )
+                )
+            ).all()
