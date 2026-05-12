0e0a9b5545614dc386e14f98e59fdc7457f0a97c
Jorgvt <vila_tronik@hotmail.com>
2026-05-12 11:58:59 +0200

    experiments

--- NA_Experiments/prop5.py
+++ NA_Experiments/prop5.py
@@ -1,10 +1,14 @@
+import matplotlib.pyplot as plt
 from visturing.properties import prop5
 
 def calculate_diffs(img1, img2):
     return ((img1-img2).mean(axis=(1,2,3))**2)**(1/2)
 
-diffs, pearson, order = prop5.evaluate(calculate_diffs,
-                                     data_path='../Data/Experiment_3/',
+output = prop5.evaluate(calculate_diffs,
+                                     data_path='../Data/Experiment_5/',
                                      gt_path='../Data/ground_truth/')
-print("Pearson correlation: ", pearson)
-print("Order: ", order)
+print(output)
+plt.figure()
+for d in output["diffs"]:
+    plt.plot(d)
+plt.show()
